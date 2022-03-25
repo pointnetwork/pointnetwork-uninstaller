e36c715ef774138147d879863d24b44679e6e77b
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-03-25 11:43:27 +0530

    Create the required bridge methods

--- electron/bridge.ts
+++ electron/bridge.ts
@@ -1,6 +1,13 @@
 import { contextBridge, ipcRenderer } from 'electron'
+import { UNINSTALL_START, UNINSTALL_CLOSE } from './channels'
 
 export const api = {
+  startInstallation: () => {
+    ipcRenderer.send(UNINSTALL_START)
+  },
+  closeInstaller: () => {
+    ipcRenderer.send(UNINSTALL_CLOSE)
+  },
   on: (channel: string, callback: Function) => {
     ipcRenderer.on(channel, (_, data) => callback(data))
   },
