f569a5d5a9be2a84df1f46649177c31f284c14cf
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-03-25 13:38:52 +0530

    Make kill command cross-platform and refactor kill logic

--- electron/main.ts
+++ electron/main.ts
@@ -7,6 +7,7 @@ import {
   UNINSTALL_LOG,
 } from './channels'
 import { exec } from 'child_process'
+import { platform } from 'process'
 import type { Process } from '../src/@types/process'
 
 const fs = require('fs')
@@ -40,9 +41,26 @@ function createWindow() {
   })
 }
 
+// Function to send the logs to the UI
 const log2UI = (...args: any) => {
   console.log(...args)
   mainWindow!.webContents.send(UNINSTALL_LOG, args)
+}
+
+// Function to kill the processes while also sending kill logs to UI
+const killCmd = platform === 'win32' ? 'taskkill /F /PID' : 'kill'
+const killProcesses = async (processes: Process[], label: string) => {
+  if (processes.length > 0) {
+    log2UI(`Stopping running processes for PointNetwork ${label}`)
+    for (const p of processes) {
+      log2UI('Stopping PointNetwork Browser process ', p.pid)
+      await exec(`${killCmd} ${p.pid}`)
+      log2UI('Stopped PointNetwork Browser process ', p.pid)
+    }
+    log2UI(`Stopped running processes for PointNetwork ${label}`)
+  } else {
+    log2UI(`No running processes for PointNetwork ${label} found`)
+  }
 }
 
 async function registerListeners() {
@@ -55,51 +73,21 @@ async function registerListeners() {
       log2UI('Starting uninstallation')
       // Find firefox and kill any running processes
       log2UI('Finding running processes for PointNetwork Browser')
-      let processes: Process[] = await find('name', /firefox/i)
-      const pointBrowserParentProcesses = processes.filter(
-        p => p.cmd.includes('point-browser') && !p.cmd.includes('tab')
+      let processes: Process[] = (await find('name', /firefox/i)).filter(
+        (p: Process) =>
+          p.cmd.includes('point-browser') && !p.cmd.includes('tab')
       )
-      if (pointBrowserParentProcesses.length > 0) {
-        log2UI('Stopping running processes for PointNetwork Browser')
-        for (const p of pointBrowserParentProcesses) {
-          log2UI('Stopping PointNetwork Browser process ', p.pid)
-          await exec(`taskkill /F /PID ${p.pid}`)
-          log2UI('Stopped PointNetwork Browser process ', p.pid)
-        }
-        log2UI('Stopped running processes for PointNetwork Browser')
-      } else {
-        log2UI('No running processes for PointNetwork Browser found')
-      }
+      await killProcesses(processes, 'Browser')
 
       // Find PointNetwork Node and kill any running processes
       log2UI('Finding running processes for PointNetwork Node')
       processes = await find('name', 'point', true)
-      if (processes.length > 0) {
-        log2UI('Stopping running processes for PointNetwork Node')
-        for (const p of processes) {
-          log2UI('Stopping PointNetwork Node process ', p.pid)
-          await exec(`taskkill /F /PID ${p.pid}`)
-          log2UI('Stopped PointNetwork Node process ', p.pid)
-        }
-        log2UI('Stopped running processes for PointNetwork Node')
-      } else {
-        log2UI('No running processes for PointNetwork Node found')
-      }
+      await killProcesses(processes, 'Node')
 
       // Find PointNetwork Dashboard and kill any running processes
       log2UI('Finding running processes for PointNetwork Dashboard')
       processes = await find('name', 'pointnetwork-dashboard', true)
-      if (processes.length > 0) {
-        log2UI('Stopping running processes for PointNetwork Dashboard')
-        for (const p of processes) {
-          log2UI('Stopping PointNetwork Dashboard process ', p.pid)
-          await exec(`taskkill /F /PID ${p.pid}`)
-          log2UI('Stopped PointNetwork Dashboard process ', p.pid)
-        }
-        log2UI('Stopped running processes for PointNetwork Dashboard')
-      } else {
-        log2UI('No running processes for PointNetwork Dashboard found')
-      }
+      await killProcesses(processes, 'Dashboard')
 
       // Remove the .point directory
       log2UI(
@@ -130,10 +118,7 @@ async function registerListeners() {
       }
 
       log2UI('Uninstall finished successfully')
-      setTimeout(() => {
-        console.log(UNINSTALL_FINISH)
-        mainWindow!.webContents.send(UNINSTALL_FINISH)
-      }, 2000)
+      mainWindow!.webContents.send(UNINSTALL_FINISH)
     } catch (error) {
       console.log(error)
     }
