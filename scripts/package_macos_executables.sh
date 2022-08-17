c5d0797519064db9e66c8a8b3898b962eb591459
Koochr <koochr@protonmail.com>
2022-08-17 16:32:52 +0100

    Change build scripts

--- scripts/package_macos_executables.sh
+++ scripts/package_macos_executables.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# This script will rename, add execution permissions and compress point executables.
-
-MAC_FILE=./out/pointnetwork-uninstaller-darwin-x64/pointnetwork-uninstaller.app
-
-mkdir -p ./out/mac_executables/pkg
-chmod +x $MAC_FILE
-mv $MAC_FILE ./out/mac_executables/pkg/pointnetwork-uninstaller.app
-tar -czvf ./out/mac_executables/pointnetwork-uninstaller.tar.gz -C ./out/mac_executables/pkg ./pointnetwork-uninstaller.app
+mkdir -p ./out/mac_executables
+cd out
+tar -czvf ./mac_executables/pointnetwork-uninstaller-darwin-x64.tar.gz ./pointnetwork-uninstaller-darwin-x64
--- scripts/package_win_executables.sh
+++ scripts/package_win_executables.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# This script will rename, add execution permissions and compress point executables.
-
-VERSION=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)
-
-WIN_FILE="./out/make/squirrel.windows/x64/pointnetwork-uninstaller-${VERSION} Setup.exe"
-
-mkdir -p ./out/win_executables/pkg
-chmod +x "$WIN_FILE"
-mv "$WIN_FILE" ./out/win_executables/pkg/pointnetwork-uninstaller.exe
-7z a ./out/win_executables/pointnetwork-uninstaller.zip ./out/win_executables/pkg/pointnetwork-uninstaller.exe
+mkdir -p ./out/win_executables
+cd out
+tar -czvf ./win_executables/pointnetwork-uninstaller-win32-x64.tar.gz ./pointnetwork-uninstaller-win32-x64
