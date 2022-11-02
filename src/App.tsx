ea6b277db2a16a085dff9cff697b748d34f619d6
Koochr <koochr@protonmail.com>
2022-11-02 23:34:06 +0000

    Add uninstalling software

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { UNINSTALL_FINISH, UNINSTALL_STARTED } from '../electron/channels'
 export function App() {
   const [isCheckboxChecked, setIsCheckboxChecked] = useState<boolean>(false)
   const [whichStep, setWhichStep] = useState<number>(1)
+  const [errors, setErrors] = useState<string[]>([])
 
   const beginUninstallation = () => {
     window.Main.startInstallation()
@@ -20,7 +21,10 @@ export function App() {
 
   useEffect(() => {
     window.Main.on(UNINSTALL_STARTED, () => setWhichStep(2))
-    window.Main.on(UNINSTALL_FINISH, () => setWhichStep(3))
+    window.Main.on(UNINSTALL_FINISH, (_errors: string[]) => {
+      setWhichStep(3)
+      setErrors(_errors)
+    })
   }, [])
 
   return (
@@ -34,7 +38,7 @@ export function App() {
           />
         )}
         {whichStep === 2 && <UninstallScreen />}
-        {whichStep === 3 && <SuccessScreen />}
+        {whichStep === 3 && <SuccessScreen errors={errors} />}
       </Box>
     </UIThemeProvider>
   )
--- src/screens/Success.tsx
+++ src/screens/Success.tsx
@@ -2,15 +2,19 @@
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import { FunctionComponent } from 'react'
 
-export default function Success() {
+const Success: FunctionComponent<{ errors: string[] }> = ({ errors }) => {
   return (
     <>
       <Typography variant="h4">Point Network Uninstaller</Typography>
-      <Typography marginTop={2} marginBottom={30}>
-        Uninstalled successfully
+      <Typography marginTop={2} marginBottom={2}>
+        Uninstalled {errors.length > 0 ? 'with errors' : 'successfully'}
       </Typography>
-      <Box display="flex" justifyContent="center">
+      {errors.map((error, index) => (
+        <Typography color={'indianred'} key={index}>{error}</Typography>
+      ))}
+      <Box display="flex" justifyContent="center" marginTop={30}>
         <Button variant="contained" onClick={window.Main.closeInstaller}>
           CLOSE
         </Button>
@@ -18,3 +22,5 @@ export default function Success() {
     </>
   )
 }
+
+export default Success
